7c41250ba5bb71909759be57252681745046496a
MarinaByhalova <mzhirnovsk@gmail.com>
2024-08-10 19:02:45 +0300

    Add files via upload
    Кортежи нельзя изменять, потому что они были разработаны таким образом.  Они относятся  к неизменяемым типам данных.

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1,2,3,'x','y')
+print("Immutable_var:", immutable_var)
+#immutable_var[o] = 5  Кортежи—это упорядоченные коллекции элементов, которые являются неизменяемыми.
+mutable_list = ['one','two','three']
+print("Mutable_list:", mutable_list)
+mutable_list[0] = 'five'
+print("Mutable_list:", mutable_list)
